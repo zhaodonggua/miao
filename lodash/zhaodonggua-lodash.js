9a1cc25dc192042dc34df332ad308c9deb2d0e25
zhaozecheng <zhaozecheng0812@gmail.com>
2022-01-10 16:03:16 +0800

    update

--- lodash/zhaodonggua-lodash.js
+++ lodash/zhaodonggua-lodash.js
@@ -1,6 +1,18 @@
 var zhaodonggua = {
-    chunk: function (array, size) {
-
+    chunk: function chunk(array, size) {
+        let arr = []
+        let length = Math.ceil(array.length / size)
+        //外围数组个数
+        for (let i = 0; i < length; i++) {
+            arr[i] = new Array()
+            //内部数组个数
+            for (let j = 0; j < size; j++) {
+                let num = i * size + j
+                if (array[num] == undefined) break;
+                arr[i].push(array[num])
+            }
+        }
+        return arr
     }
 
 }
